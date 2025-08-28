77264079ad442497a4f8f9b549f9eefa35ce95f0
Spencer-Lee-Barry <spencer.lee.barry@gmail.com>
2025-08-28 16:25:51 -0700

    getting env vars in python

--- .github/workflows/getUnityPackageVersion.py
+++ .github/workflows/getUnityPackageVersion.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import os.path
 from unityVersionHelpers import getPackageVersionStr
 
@@ -6,6 +7,7 @@ def main():
     # Create command-line argument parser
     parser = argparse.ArgumentParser(description="Build a Unity project")
     parser.add_argument('path_to_package', type=str, help='The path to the Unity package json file')
+    parser.add_argument('env_vars', type=str, help='Environment variables name')
 
     # Parse arguments
     args = parser.parse_args()
@@ -14,7 +16,9 @@ def main():
         raise Exception(f"Path to package.json not valid: {args.path_to_package}")
 
     version = getPackageVersionStr(args.path_to_package)
-    return version
+    envFilePath = os.getenv(parser.env_vars)
+    with open(envFilePath, 'a') as envFile:
+        envFile.write(f"PACKAGE_VERSION={version}")
 
 if __name__ == "__main__":
     main()
